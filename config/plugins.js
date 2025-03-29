dc601983156b14ba9864c162d1bf652c25019a53
dharmikvarsani <dharmikvarsani968@gmail.com>
2025-03-29 13:13:06 +0530

    'add-cloudnary'

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,13 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: "@strapi/provider-upload-cloudinary",
+        providerOptions: {
+          cloud_name: env("drhlytkto"),
+          api_key: env("972785359645731"),
+          api_secret: env("8dnBGPfBUXLLhu5VKlOqkMf4pZc"),
+        },
+      },
+    },
+  });
+  
